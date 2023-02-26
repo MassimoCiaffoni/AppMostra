b193518756b3261b2ed6cb3835491fa11eb0f227
Massimo <ciaffoni.massimo98@gmail.com>
2023-02-26 15:26:16 +0100

    aggiunta zoom ed immagini

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var canvas = document.getElementById("museum-map");
 
 
 
-
 canvas.addEventListener("touchend", function(event) {
   event.preventDefault(); // previene lo scrolling della pagina quando si tocca il canvas
 });
@@ -30,14 +29,91 @@ for (var i = 0; i < highlights.length; i++) {
         <button id="next" class="next-button">&gt;</button>
       `;
 
+
       var previousBtn = document.getElementById("previous");
       var nextBtn = document.getElementById("next");
+      var lightboxImage = document.querySelector('.lightbox-image');
+      const doubleTouchDelay = 250;
+      let lastTouchStart = 0;
+      let isZoomed= false;
+      // Store previous touch position
+      let prevTouch = {x: 0, y: 0};
+
+      // Add touchmove event listener
+      lightboxImage.addEventListener('touchmove', (e) => {
+        if (isZoomed) {
+          // Get current touch position
+          const touch = {x: e.touches[0].pageX, y: e.touches[0].pageY};
+          // Get current transform values
+          const transform = window.getComputedStyle(lightboxImage).getPropertyValue('transform');
+          const matrix = transform.match(/^matrix\((.+)\)$/i)[1].split(',').map(parseFloat);
+          const scaleX = matrix[0];
+          const currentX = matrix[4];
+          const currentY = matrix[5];
+          // Calculate movement amount
+          const dx = touch.x - prevTouch.x;
+          const dy = touch.y - prevTouch.y;
+          // Apply new transform values
+          lightboxImage.style.transform = `translate(${currentX + dx}px, ${currentY + dy}px) scale(${scaleX})`;
+          // Update previous touch position
+          prevTouch = touch;
+          console.log(prevTouch)
+          
+        }
+      });
+
+
+
+      lightboxImage.addEventListener('touchstart', (e) => {
+        const now = Date.now();
+        const timeDiff = now - lastTouchStart;
+        lastTouchStart = now;
+        if (timeDiff < doubleTouchDelay) {
+          const touch = e.touches[0];
+          const rect = lightboxImage.getBoundingClientRect();
+          const x = touch.clientX - rect.left;
+          const y = touch.clientY - rect.top;
+          const transform = window.getComputedStyle(lightboxImage).getPropertyValue('transform');
+          console.log(transform)
+          const matrix = transform.match(/^matrix\((.+)\)$/i)[1].split(',').map(parseFloat);
+          const scaleX = matrix[0];
+          const scaleY = matrix[3];
+          console.log(scaleX, scaleY);
+          const currentScale = scaleX;
+          const targetScale = 2.0;
+          const scaleDiff = targetScale - scaleX;
+          const newScale = `scale(${targetScale})`;
+          // Calculate the new transform origin
+          const offsetX = (x * scaleDiff) / targetScale;
+          const offsetY = (y * scaleDiff) / targetScale;
+          const newOrigin = `${x}px ${y}px`;
+          if (currentScale==1) {
+            console.log(images[currentImageIndex]);
+            if (images[currentImageIndex]=='images/registro matricola detenuti-2.jpg'){
+              lightboxImage.style.transform = `scale(4) translate(-${offsetX}px, -${offsetY}px)`;
+              isZoomed=true
+              lightboxImage.style.transformOrigin=newOrigin
+            }
+            else{
+              lightboxImage.style.transform = `${newScale} translate(-${offsetX}px, -${offsetY}px)`;
+              isZoomed=true
+              lightboxImage.style.transformOrigin=newOrigin
+            }
+          }
+          else {
+            lightboxImage.style.transform = `scale(1)`
+            isZoomed=false
+          }
+        }
+      });
 
       previousBtn.addEventListener("touchstart", function() {
         currentImageIndex--;
         if (currentImageIndex < 0) {
           currentImageIndex = images.length - 1;
         }
+        lightboxImage.style.transform = `scale(1)`
+        isZoomed= false
         content.querySelector("img").src = images[currentImageIndex];
       });
 
@@ -46,6 +122,8 @@ for (var i = 0; i < highlights.length; i++) {
         if (currentImageIndex >= images.length) {
           currentImageIndex = 0;
         }
+        lightboxImage.style.transform = `scale(1)`
+        isZoomed= false
         content.querySelector("img").src = images[currentImageIndex];
       });
     }
